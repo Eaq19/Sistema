dbde6bbbf27f5fe706cb1f748f417851421737b7
Edison <eandresk.13@hotmail.com>
2016-07-26 13:43:36 -0500

    Realizacion del diseño dividiendolo por parte, definir variables y instalacion de paypal y phpmailer

---
+++ core/core.php
@@ -0,0 +1,10 @@
+<?php 
+	/*
+		El nucleo del sistema
+	*/
+	define('HTML_DIR', 'html/');
+	define('APP_TITLE', 'Ed');
+	define('APP_COPY', 'Copryright &copy: ' . date('Y', time()) . ' Ed Software.');
+
+	require('vendor/autoload.php');
+?>
